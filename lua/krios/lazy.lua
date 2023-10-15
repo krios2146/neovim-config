d9547b05c632dcd269ae55660119922587642d84
Vladimir Kidyaev <krios2146@gmail.com>
2023-10-15 15:46:16 +0700

    feat: add colorscheme

--- lua/krios/lazy.lua
+++ lua/krios/lazy.lua
@@ -14,5 +14,7 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
 	{ 'nvim-telescope/telescope.nvim', tag = '0.1.4' },
 	'nvim-lua/plenary.nvim',
-	{"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"}
+	{"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
+	"rebelot/kanagawa.nvim"
 })
+vim.cmd("colorscheme kanagawa")
