8cfb7628b0223ac62aa7a956674c086c7daae9fd
Vladimir Kidyaev <krios2146@gmail.com>
2023-10-15 17:41:20 +0700

    chore: fix identations in the lazy.lua

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 Uses [lazy.nvim](https://github.com/folke/lazy.nvim) as plugin manager
 
 Telescope plugin requires manual installation of
+
  - [ripgrep](https://github.com/BurntSushi/ripgrep)
  - [fd](https://github.com/sharkdp/fd)
 
--- lua/krios/lazy.lua
+++ lua/krios/lazy.lua
@@ -1,14 +1,14 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-	  vim.fn.system({
-		  "git",
-		  "clone",
-		  "--filter=blob:none",
-		  "https://github.com/folke/lazy.nvim.git",
-		  "--branch=stable", -- latest stable release
-		  lazypath,
-	  })
-  end
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
+end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
@@ -17,4 +17,6 @@ require("lazy").setup({
 	{"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
 	"rebelot/kanagawa.nvim"
 })
+
 vim.cmd("colorscheme kanagawa")
+
