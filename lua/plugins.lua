b50549c88ef1237d034a3f7a7e09625680a0bc99
Vladimir Kidyaev <krios2146@gmail.com>
2023-10-19 21:53:00 +0700

    feat: add vim-fugitive plugin

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -2,5 +2,6 @@ return {
 	{ 'nvim-telescope/telescope.nvim', tag = '0.1.4' },
 	'nvim-lua/plenary.nvim',
 	"rebelot/kanagawa.nvim",
-	"mbbill/undotree"
+	"mbbill/undotree",
+	"tpope/vim-fugitive"
 }
