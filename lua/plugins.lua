ec2864d8d6074afada02674ca8ec6faec0280c91
krios2146 <krios2146@gmail.com>
2023-12-12 21:16:43 +0700

    feat: add auto-pairs plugin

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -12,4 +12,8 @@ return {
     {'hrsh7th/nvim-cmp'},
     {'L3MON4D3/LuaSnip'},
     {'wakatime/vim-wakatime'},
+    {'windwp/nvim-autopairs',
+        event = "InsertEnter",
+        opts = {} -- this is equalent to setup({}) function
+    },
 }
