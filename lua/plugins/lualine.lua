ae3316780b506a70a644a7746caece06a200130e
krios2146 <krios2146@gmail.com>
2023-12-14 16:14:49 +0700

    feat: update config for the lualine plugin

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,6 +1,13 @@
 return {{
     'nvim-lualine/lualine.nvim',
     config = function()
-        require('lualine').setup()
+        require('lualine').setup({
+            sections = {
+                lualine_x = { 
+                    { 'datetime', style = '%H:%M:%S' }, 
+                    'filetype'
+                }
+            }
+        })
     end
 }}
