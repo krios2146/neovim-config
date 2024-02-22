9e3803704f487f6e832456a55e928206caf187e0
krios2146 <krios2146@gmail.com>
2024-02-22 13:29:07 +0700

    feat: update telescope.lua with descriptions for mappings

--- lua/config/telescope.lua
+++ lua/config/telescope.lua
@@ -1,8 +1,12 @@
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-vim.keymap.set('n', '<leader>ps', function()
-	builtin.grep_string({ search = vim.fn.input("Grep > ") });
-end)
-vim.keymap.set('n', '<leader>of', builtin.oldfiles, {})
+
+vim.keymap.set('n', '<leader>pf', builtin.find_files, {desc = '[P]roject [F]iles'})
+vim.keymap.set('n', '<leader>gf', builtin.git_files, {desc = '[G]it [F]iles'})
+vim.keymap.set('n', '<leader>ps', 
+    function()
+        builtin.grep_string({ search = vim.fn.input("Grep > ") });
+    end, 
+    {desc = '[P]roject [S]earch'}
+)
+vim.keymap.set('n', '<leader>of', builtin.oldfiles, {desc = '[O]ld [Files]'})
 
