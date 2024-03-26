101655bf2ec59d93fe188fa49bd2e2dea31e25b5
krios2146 <krios2146@gmail.com>
2024-03-26 21:19:36 +0700

    fix: treesitter config & add gleam to it

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,16 +1,18 @@
-return {{
-	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
-	config = function()
-		require("nvim-treesitter.install").prefer_git = true
-		local configs = require("nvim-treesitter.configs")
+return { {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+        require("nvim-treesitter.install").prefer_git = true
+        local configs = require("nvim-treesitter.configs")
 
-		configs.setup({
-			ensure_installed = { "lua", "python", "rust" },
-			sync_installed = false,
+        configs.setup({
+            ensure_installed = { "lua", "python", "gleam" },
+            sync_installed = false,
             auto_install = true,
-			highlight = { enabled = true },
-			indent = { enabled = true }
-		})
-	end
-}}
+            highlight = { enable = true },
+            indent = { enable = true },
+            incremental_selection = { enable = true },
+            textobjects = { enable = true },
+        })
+    end,
+} }
