3fc084825eb1d3336a343cc75beb21ffbcb1595e
krios2146 <krios2146@gmail.com>
2024-04-15 23:57:23 +0700

    feat: add more themes

--- lua/themes.lua
+++ lua/themes.lua
@@ -1,4 +1,11 @@
 return {
   { 'folke/tokyonight.nvim' },
   { 'marko-cerovac/material.nvim' },
+  { 'ray-x/aurora' },
+  { 'rebelot/kanagawa.nvim' },
+  { 'catppuccin/nvim', name = 'catppuccin' },
+  { 'Shatur/neovim-ayu' },
+  { 'nanotech/jellybeans.vim' },
+  { 'ayu-theme/ayu-vim' },
+  { 'rose-pine/neovim', name = 'rose-pine' },
 }
