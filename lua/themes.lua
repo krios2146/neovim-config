ccfd15651f7bf81f1e92af4e6abd3c1f10463544
krios2146 <krios2146@gmail.com>
2024-04-16 01:16:18 +0700

    refactor: delete old ayu theme & configure line highlights of the newer ayu theme

--- lua/themes.lua
+++ lua/themes.lua
@@ -4,8 +4,20 @@ return {
   { 'ray-x/aurora' },
   { 'rebelot/kanagawa.nvim' },
   { 'catppuccin/nvim', name = 'catppuccin' },
-  { 'Shatur/neovim-ayu' },
+  {
+    'Shatur/neovim-ayu',
+    config = function()
+      require('ayu').setup {
+        overrides = {
+          CursorLine = { bg = '#111822' },
+          CursorLineNr = { bg = '#111822' },
+          CursorColumn = { bg = '#111822' },
+          ColorColumn = { bg = '#111822' },
+          CursorLineConceal = { bg = '#111822' },
+        },
+      }
+    end,
+  },
   { 'nanotech/jellybeans.vim' },
-  { 'ayu-theme/ayu-vim' },
   { 'rose-pine/neovim', name = 'rose-pine' },
 }
